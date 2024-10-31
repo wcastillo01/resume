183319f11e4b863b4418232e0b97e2937762dbfa
Wilson Castillo <wcastillo1@MacBook-Pro-de-Wilson-2.local>
2024-10-31 10:10:11 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export function Header(){ 
+    return (
+        <div>      
+            Logo      
+            whoami
+            contacto
+            certificaciones
+            hoja de vida
+        </div>
+    );
+
+}
+
+export default Header;
