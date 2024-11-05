c99a1ad81319c56e1b858f95bfa8a329e2a51152
Wilson Castillo <wcastillo1@MacBook-Pro-de-Wilson-2.local>
2024-11-04 22:26:13 -0400

    feat: add nav bar items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 export function Header(){ 
     return (
-        <div>      
-            Logo      
-            whoami
-            contacto
-            certificaciones
-            hoja de vida
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div>
+                <p>Logo</p>
+            </div>
+
+            <div className="justify-content: end">      
+
+                <ul style={{listStyle: 'none', display: "flex", gap: "2rem"}}>
+                    <a href="#"><li>whoami</li></a>
+                    <a href="#"> <li>hoja de vida</li></a>
+                    <a href="#"><li>certificaciones</li></a>
+                    <a href="#"><li>contacto</li></a>
+                </ul>
+              
+            </div>
         </div>
     );
 
